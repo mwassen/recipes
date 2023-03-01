b52ab21a7bde07896851270f7b4cf4ad6355c767
mwassen <marcuswassen@gmail.com>
2023-03-01 21:33:10 +0100

    Add frequency to tags

--- src/utils/getTags.ts
+++ src/utils/getTags.ts
@@ -2,3 +2,11 @@ import type { CollectionEntry } from "astro:content";
 
 export const getTags = (recipes: CollectionEntry<"recipes">[]) =>
   [...new Set(recipes.flatMap((recipe) => recipe.data.tags))].sort();
+
+export const getTagsWithFrequency = (recipes: CollectionEntry<"recipes">[]) =>
+  getTags(recipes)
+    .map((o) => ({
+      name: o,
+      frequency: recipes.filter((r) => r.data.tags.includes(o)).length,
+    }))
+    .sort((a, b) => b.frequency - a.frequency);
