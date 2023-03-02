22cbfe5aa8c51f35fa3b72eb23338ae7f50b086b
Marcus Wassén <marcuswassen@gmail.com>
2023-03-02 11:23:02 +0100

    Remove boilerplate comment

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,8 +6,7 @@ const recipesCollection = defineCollection({
     date: z.date().transform((d) => new Date(d)),
   }),
 });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+
 export const collections = {
   recipes: recipesCollection,
 };
